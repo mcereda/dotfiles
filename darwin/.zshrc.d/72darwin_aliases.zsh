2ea8dfdb65e3c42a654d1a44885befc580bd8b9c
Michele Cereda <cereda.michele@gmail.com>
2021-10-11 23:51:39 +0200

    Imported the darwin package

---
+++ darwin/.zshrc.d/72darwin_aliases.zsh
@@ -0,0 +1,10 @@
+alias broken-links='find . -type l -not -path "*/Library/*" -exec test ! -e {} \; -print'
+alias dead-links='broken-links'
+
+# Enable colors.
+alias grep='grep --color=always'
+alias ls='ls -G'
+
+# Get the number of available threads in the system.
+# nproc should be available by default, but if not just uncomment this.
+# alias nproc='sysctl -n hw.logicalcpu'
