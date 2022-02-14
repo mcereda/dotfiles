945823cfe379d682f8c2daf56236d5e67deebb76
Michele Cereda <cereda.michele@gmail.com>
2022-02-14 10:32:48 +0100

    Removed deprecated python upgrade alias

--- zsh/.zshrc.d/75zsh_aliases.zsh
+++ zsh/.zshrc.d/75zsh_aliases.zsh
@@ -1,9 +1,5 @@
 alias alert='notify-send --urgency=low -i "$([ $? = 0 ] && echo terminal || echo error)" "$(history|tail -n1|sed -e '\''s/^\s*[0-9]\+\s*//;s/[;&|]\s*alert$//'\'')"'
-
-alias pip3-upgrade="pip3 list --user --outdated --format=json | jq -r '.[].name' - | xargs --verbose pip3 install --user --upgrade"
-
 alias please='sudo'
-
 alias uncomment='grep -Ev "^#|^$"'
 
 # History.
