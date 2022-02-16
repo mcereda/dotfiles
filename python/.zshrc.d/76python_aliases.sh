e07c7c7e8ebe4e908006d54065d65b918d82c11c
Michele Cereda <cereda.michele@gmail.com>
2022-02-16 11:08:59 +0100

    Improved python aliases

--- python/.zshrc.d/76python_aliases.sh
+++ python/.zshrc.d/76python_aliases.sh
@@ -1,1 +1,2 @@
-alias python-upgrade-all='pip install --requirement <(pip freeze | sed "s/==/>=/") --upgrade'
+alias python-upgrade-all='pip install --no-cache-dir --requirement <(pip freeze | sed "s/==/>=/") --upgrade'
+alias python-user-upgrade-all='python-upgrade-all --user'
