8ea699d1eb631f31ffafe411157b942356f44518
Michele Cereda <cereda.michele@gmail.com>
2022-11-05 21:26:42 +0100

    Imported zypper shortcuts

---
+++ .chezmoitemplates/zypper.sh
@@ -0,0 +1,11 @@
+##########
+# Zypper.
+##########
+
+alias zypper-system-upgrade-routine='sudo zypper refresh && sudo zypper dist-upgrade && sudo zypper ps --short'
+
+# Add the upgrade routine to the system's.
+if ! [[ "${UPGRADE_ROUTINES[@]}" =~ 'snap-upgrade-routine' ]]
+then
+	UPGRADE_ROUTINES+='zypper-system-upgrade-routine'
+fi
