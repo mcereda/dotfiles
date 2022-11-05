7f59767df9b4f0d0c5b292023878898962c60a54
Michele Cereda <cereda.michele@gmail.com>
2022-11-05 13:36:54 +0100

    Minor fixes

--- .chezmoitemplates/homebrew.sh
+++ .chezmoitemplates/homebrew.sh
@@ -1,8 +1,12 @@
 alias brew-kegs='brew info --json=v1 --installed | jq -r "map(select(.linked_keg == null) | .name)[]"'
 alias brew-upgrade-routine='brew update --verbose && brew upgrade && brew cleanup --prune all -s'
 
+# Prioritize kegs.
+echo ${HOMEBREW_KEGS[@]} | xargs -I{} echo "${HOMEBREW_PREFIX}/opt/{}/bin" \
+| xargs path-prepend
+
 # Add the upgrade routine to the system's.
-if [[ ! "${UPGRADE_ROUTINES[@]}" =~ 'brew-upgrade-routine' ]]
+if ! [[ "${UPGRADE_ROUTINES[@]}" =~ 'brew-upgrade-routine' ]]
 then
 	UPGRADE_ROUTINES+='brew-upgrade-routine'
 fi
