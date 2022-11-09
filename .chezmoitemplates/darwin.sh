c6da186296f8962817fbb89c5f9f7be7fb710666
Michele Cereda <cereda.michele@gmail.com>
2022-11-09 16:49:30 +0100

    Added Darwin-specific shell aliases and functions

---
+++ .chezmoitemplates/darwin.sh
@@ -0,0 +1,24 @@
+alias broken-links='find . -type l -not -path "*/Library/*" -exec test ! -e {} \; -print'
+alias dead-links='broken-links'
+
+alias darwin-system-upgrade-routine='softwareupdate --install --recommended'
+
+# Enable colors.
+alias grep='grep --color=always'
+alias ls='ls -G'
+
+{{- if lookPath "gdiff" }}
+alias diff='gdiff --color=always'
+{{- end }}
+
+# Get the number of available threads in the system.
+# nproc should be available by default, but if not just uncomment this.
+# alias nproc='sysctl -n hw.logicalcpu'
+
+# Add the upgrade routine to the system's.
+if ! [[ "${UPGRADE_ROUTINES[@]}" =~ 'darwin-system-upgrade-routine' ]]
+then
+	UPGRADE_ROUTINES+='darwin-system-upgrade-routine'
+fi
+
+{{- /* This line just removes all the extra space */ -}}
