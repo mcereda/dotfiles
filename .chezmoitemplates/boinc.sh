5903f6e12c14ff0c3f50328ed0690150ff7c01fe
Michele Cereda <cereda.michele@gmail.com>
2022-11-10 11:53:49 +0100

    Added BOINC-specific shell additions

---
+++ .chezmoitemplates/boinc.sh
@@ -0,0 +1,13 @@
+{{- if and
+      (lookPath "systemctl")
+      (lookPath "cpupower") }}
+
+alias boinc-down='\
+	sudo systemctl stop boinc-client.service && \
+	sudo cpupower frequency-set --governor schedutil'
+alias boinc-up='\
+	sudo cpupower frequency-set --governor ondemand && \
+	echo 1 | sudo tee /sys/devices/system/cpu/cpufreq/ondemand/ignore_nice_load && \
+	sudo systemctl start boinc-client.service'
+
+{{- end }}
