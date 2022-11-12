c13c30a46cf03c88dcb1c0301628ecdaaa3d4cfb
Michele Cereda <cereda.michele@gmail.com>
2022-11-12 15:41:04 +0100

    Improved kubernetes-specific shell additions

--- .chezmoitemplates/kubernetes.sh
+++ .chezmoitemplates/kubernetes.sh
@@ -1,49 +1,72 @@
-{{-    if lookPath "az" }}
+alias kubectl-current-context='kubectl config current-context'
 
-alias aks-versions='\
-	az aks get-versions --output table \
-		--location ${AZURE_LOCATION:?required but not set}'
+kubectl-decode () { echo "$1" | base64 -d ; }
+kubectl-encode () { echo -n "$1" | base64 ; }
 
-{{-    end }}
-
-alias eks-dashboard='\
-	kubectl --namespace kube-system describe secret \
-		$(kubectl --namespace kube-system get secrets | grep eks-admin | awk "{print \$1}") \
-	&& echo "http://localhost:8001/api/v1/namespaces/kube-system/services/https:kubernetes-dashboard:/proxy/ \
-	&& kubectl proxy"'
-
-kubectl-decode () { echo $1 | base64 -d ; } && alias k8s-decode='kube-decode'
-kubectl-encode () { echo -n $1 | base64 ; } && alias k8s-encode='kube-encode'
-
-kubectl-check-cluster-connection () {
-	# `kubectl cluster-info` takes too long to be used on a prompt
-
+kubectl-current-cluster () {
 	is-true "$DEBUG" && enable-xtrace
 
-	local KUBE_CONTEXT="${KUBE_CONTEXT:-$(kubectl config current-context)}"
-	local KUBE_CLUSTER="${KUBE_CLUSTER:-$(kubectl config view -o jsonpath="{.contexts[?(@.name == '${KUBE_CONTEXT}')].context.cluster}")}"
-	local KUBE_SERVER="${KUBE_SERVER:=$(kubectl config view -o jsonpath="{.clusters[?(@.name == '${KUBE_CLUSTER}')].cluster.server}")}"
-
-	curl --connect-timeout 1 --insecure --silent "$KUBE_SERVER" > /dev/null
+	kubectl config view \
+		-o jsonpath="{.contexts[?(@.name == '${KUBECONTEXT:-$(kubectl-current-context)}')].context.cluster}"
 
 	local RETURN_VALUE=$?
 	is-true "$DEBUG" && disable-xtrace
 	return $RETURN_VALUE
-} && alias k8s-check-cluster-connection='kubectl-check-cluster-connection'
+}
+
+kubectl-current-server () {
+	is-true "$DEBUG" && enable-xtrace
+
+	kubectl config view \
+		-o jsonpath="{.clusters[?(@.name == '${KUBECLUSTER:-$(kubectl-current-cluster)}')].cluster.server}"
+
+	local RETURN_VALUE=$?
+	is-true "$DEBUG" && disable-xtrace
+	return $RETURN_VALUE
+}
+
+# `kubectl cluster-info` takes too long to be used on a prompt
+kubectl-check-cluster-connection () {
+	is-true "$DEBUG" && enable-xtrace
+
+	local TIMEOUT="1"
+	curl --connect-timeout "$TIMEOUT" --insecure --silent "${KUBESERVER:-$(kubectl-current-server)}" > /dev/null
+
+	local RETURN_VALUE=$?
+	is-true "$DEBUG" && disable-xtrace
+	return $RETURN_VALUE
+}
 
 kubectl-delete-non-default-service-accounts () {
-	local K8S_NAMESPACE="${K8S_NAMESPACE:=default}"
+	is-true "$DEBUG" && enable-xtrace
 
-	kubectl --namespace "$K8S_NAMESPACE" delete serviceaccounts \
-		$(kubectl --namespace "$K8S_NAMESPACE" get serviceaccounts \
-			--output jsonpath="{.items[?(@.metadata.name!='default')].metadata.name}" \
-			| tr ' ' ',')
-} && alias k8s-delete-non-default-service-accounts='kubectl-delete-non-default-service-accounts'
+	KUBEOPTIONS+=( '--namespace' "${KUBENAMESPACE:-default}" )
 
-alias k8s-get-nodes-issues=kube-get-nodes-issues
-alias k8s-get-nodes-issues-yaml=kube-get-nodes-issues-yaml
-alias kube-get-nodes-issues='kubectl get nodes -o json | jq '\''.items[] | {"node": .metadata.name, "issues": [.status.conditions[] | select(.status != "False")]}'\'' -'
-alias kube-get-nodes-issues-yaml='kubectl get nodes -o yaml | yq -y '\''.items[] | {"node": .metadata.name, "issues": [.status.conditions[] | select(.status != "False")]}'\'' -'
+	kubectl ${KUBEOPTIONS[@]} get serviceaccounts \
+		--output jsonpath="{.items[?(@.metadata.name!='default')].metadata.name}" \
+	| tr ' ' ',' \
+	| xargs kubectl ${KUBEOPTIONS[@]} delete serviceaccounts
+
+	local RETURN_VALUE=$?
+	is-true "$DEBUG" && disable-xtrace
+	return $RETURN_VALUE
+}
+
+kubectl-nodes-with-issues () {
+	kubectl get nodes -o jsonpath='{.items[]}' \
+	| jq '
+		{
+			"node": .metadata.name,
+			"issues": [
+				.status.conditions[] | select(
+					.status != "False" and
+					.type != "Ready"
+				)
+			]
+		} | select( .issues|length > 0 )
+	' -
+}
+alias kubectl-nodes-with-issues-in-yaml='kubectl-nodes-with-issues | yq -y "." -'
 
 # # kube-ps1 prompt customization
 # # shell function, need to use built-in 'type'
@@ -53,3 +76,25 @@ alias kube-get-nodes-issues-yaml='kubectl get nodes -o yaml | yq -y '\''.items[]
 # 	export KUBE_PS1_SYMBOL_ENABLE=false KUBE_PS1_PREFIX='[' KUBE_PS1_SUFFIX=']'
 # 	export PS1='$(kube_ps1)'$PS1
 # fi
+
+{{-    if lookPath "az" }}
+
+##########
+# AKS (Microsoft Azure).
+##########
+
+alias aks-versions='\
+	az aks get-versions --output table \
+		--location ${AZURE_LOCATION:?required but not set}'
+
+{{-    end }}
+
+##########
+# EKS (Amazon Web Services).
+##########
+
+alias eks-dashboard='\
+	kubectl --namespace kube-system describe secret \
+		$(kubectl --namespace kube-system get secrets | grep eks-admin | awk "{print \$1}") \
+	&& echo "http://localhost:8001/api/v1/namespaces/kube-system/services/https:kubernetes-dashboard:/proxy/ \
+	&& kubectl proxy"'
