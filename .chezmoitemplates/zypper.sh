345ce84629c1ab2de5e51a6dc13d1d9774ccec42
Michele Cereda <cereda.michele@gmail.com>
2023-05-06 12:10:58 +0200

    fix: alias name

--- .chezmoitemplates/zypper.sh
+++ .chezmoitemplates/zypper.sh
@@ -5,7 +5,7 @@
 alias zypper-system-upgrade-routine='sudo zypper refresh && sudo zypper dist-upgrade && sudo zypper ps --short'
 
 # Add the upgrade routine to the system's.
-if ! [[ "${UPGRADE_ROUTINES[@]}" =~ 'snap-upgrade-routine' ]]
+if ! [[ "${UPGRADE_ROUTINES[@]}" =~ 'zypper-system-upgrade-routine' ]]
 then
 	UPGRADE_ROUTINES+='zypper-system-upgrade-routine'
 fi
