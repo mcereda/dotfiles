24bada293da49fb3801b3125fdbf1784c1f2ead4
Michele Cereda <cereda.michele@gmail.com>
2023-06-06 22:28:30 +0200

    fix: trailing spaces

--- .chezmoitemplates/citrix.sh
+++ .chezmoitemplates/citrix.sh
@@ -1,7 +1,7 @@
 {{- if eq .chezmoi.os "darwin" }}
 
 alias citrix-disable-autostart='\
-	find /Library/LaunchAgents /Library/LaunchDaemons -iname "*.citrix.*.plist" \	
+	find /Library/LaunchAgents /Library/LaunchDaemons -iname "*.citrix.*.plist" \
 	-exec sudo -p "sudo password: " mv -fv {} {}.backup ";"'
 
 {{- end }}
