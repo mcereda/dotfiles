00b2a783bfe8ee100ee2d121c633df113ddc1b6a
Michele Cereda <cereda.michele@gmail.com>
2026-09-12 15:48:13 +0200

    chore(python): remove global requirements in favour of uv

--- .chezmoitemplates/python.sh
+++ .chezmoitemplates/python.sh
@@ -3,24 +3,14 @@ then
 	PYTHONCACHEDIROPTIONS+=( '--no-cache-dir' )
 fi
 
-for FILE in ${PYTHONGLOBALREQUIREMENTFILES[@]}
-do
-	if [[ -r "$FILE" ]]
-	then
-		PYTHONGLOBALREQUIREMENTOPTIONS+=( '--requirement' "$FILE" )
-	fi
-done
-
 {{-    if or
          (lookPath "pip2")
          (lookPath "pip3") }}
 
 alias python-upgrade-all='pip install -U ${PYTHONCACHEDIROPTIONS[@]} --requirement <(pip freeze | sed "s/==/>=/")'
-alias python-upgrade-global-requirement='pip install -U ${PYTHONCACHEDIROPTIONS[@]} ${PYTHONGLOBALREQUIREMENTOPTIONS[@]}'
 alias python-upgrade-pip='pip install -U ${PYTHONCACHEDIROPTIONS[@]} pip'
 
 alias python-user-upgrade-all='python-upgrade-all --user'
-alias python-user-upgrade-global-requirement='python-upgrade-global-requirement --user'
 alias python-user-upgrade-pip='python-upgrade-pip --user'
 
 alias python-upgrade-routine='python-user-upgrade-global-requirement'
